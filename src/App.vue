d60885c9eaa43e98de1c2f9c7434f672465c29c9
leisuchu <363126838@qq.com>
2020-11-03 13:07:00 +0800

    vu3 测试

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div>
+    <button v-for="(girl,index) in girls" :key="index" @click="selectGirlFun(index)">
+        {{index}}--{{girl}}
+    </button>
+    <div>您选择了【{{selectGirl}}】</div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {
+    defineComponent,
+    reactive,
+    toRefs,
+    onBeforeMount,
+    onMounted,
+    onBeforeUnmount,
+    onUnmounted
+} from "vue";
 
+interface VueTestOne {
+    girls: string[];
+    selectGirl: string;
+    selectGirlFun: (index: number) => void;
+}
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    setup() {
+
+        // ref方式
+        // const girls = ref(['g1', 'g2', 'g3']);
+        // const selectGirl = ref('')
+        // const selectGirlFun = (index: number) => {
+        //     selectGirl.value = girls.value[index]
+        // }
+
+        // reactive方式
+        const data: VueTestOne = reactive({
+            girls: ['g1', 'g2', 'g3'],
+            selectGirl: '',
+            selectGirlFun: (index: number) => {
+                data.selectGirl = data.girls[index]
+            }
+        });
+
+        onBeforeMount(() => {
+            console.log('1 挂载前调用');
+        })
+        onMounted(() => {
+            console.log('2 挂载后调用')
+        })
+        onBeforeUnmount(() => {
+            console.log('3 组件销毁前调用')
+        })
+        onUnmounted(() => {
+            console.log('3 组件销毁后调用')
+        })
+        return {
+            ...toRefs(data)
+        }
+    }
+
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
